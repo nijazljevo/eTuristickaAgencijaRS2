850619d3077247b62c8c343edb6be13c6b40b31b
Dusan Peric <dusan.peric@page.ba>
2025-05-12 15:21:50 +0200

    refactor(APIService): add debug mode check for error logging

--- UI/eturistickaagencija_mobile/lib/services/APIService.dart
+++ UI/eturistickaagencija_mobile/lib/services/APIService.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import '../model/destinacija.dart';
@@ -273,7 +274,9 @@ class APIService {
         return bool.tryParse(response.body) ?? false;
       }
     } catch (e) {
-      print('Error checking rating: $e');
+      if (kDebugMode) {
+        print('Error checking rating: $e');
+      }
     }
     return false;
   }
