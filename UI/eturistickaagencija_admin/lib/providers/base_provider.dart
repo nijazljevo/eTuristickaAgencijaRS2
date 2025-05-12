a5d787a56ba4ebb92d3b1c367d38ab5955667d2e
Dusan Peric <dusan.peric@page.ba>
2025-05-12 15:08:49 +0200

    fix: implement rating check in DestinacijaDetails
    Implemented a check to prevent users from rating a destination more than once in DestinacijaDetails.

--- UI/eturistickaagencija_admin/lib/providers/base_provider.dart
+++ UI/eturistickaagencija_admin/lib/providers/base_provider.dart
@@ -5,121 +5,123 @@ import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 import '../models/search_result.dart';
 
-abstract class BaseProvider<T> with ChangeNotifier{
+abstract class BaseProvider<T> with ChangeNotifier {
   static String? _baseUrl;
-  String _endpoint="";
-  BaseProvider(String endpoint){
-    _endpoint=endpoint;
-    _baseUrl=const String.fromEnvironment("baseUrl",defaultValue: "http://localhost:7073/");
+  String _endpoint = "";
+  BaseProvider(String endpoint) {
+    _endpoint = endpoint;
+    _baseUrl = const String.fromEnvironment("baseUrl",
+        defaultValue: "http://192.168.1.5:5001/");
   }
-  Future<SearchResult<T>>get( {dynamic filter})async{
-    var url="$_baseUrl$_endpoint";
+  Future<SearchResult<T>> get({dynamic filter}) async {
+    var url = "$_baseUrl$_endpoint";
 
-    if(filter!=null){
-      var queryString=getQueryString(filter);
-      url="$url?$queryString";
-
+    if (filter != null) {
+      var queryString = getQueryString(filter);
+      url = "$url?$queryString";
     }
 
-    var uri=Uri.parse(url);
-    var headers=createHeaders();
+    var uri = Uri.parse(url);
+    var headers = createHeaders();
 
-    var response=await http.get(uri,headers: headers);
+    var response = await http.get(uri, headers: headers);
 
-
-    print("response: ${response.request} ${response.statusCode} ${response.body}");
-   if(isValidResponse(response)){
-     var data=jsonDecode(response.body);
-     var result=SearchResult<T>();
-    // result.count=data['count'];
-     for(var item in data){
-      
-      result.result.add(fromJson(item));
-     }
-    return result;
-   }
-   else{
-    // ignore: unnecessary_new
-    throw new Exception("Unknown error");
-   }
+    print(
+        "response: ${response.request} ${response.statusCode} ${response.body}");
+    if (isValidResponse(response)) {
+      var data = jsonDecode(response.body);
+      var result = SearchResult<T>();
+      // result.count=data['count'];
+      for (var item in data) {
+        result.result.add(fromJson(item));
+      }
+      return result;
+    } else {
+      // ignore: unnecessary_new
+      throw new Exception("Unknown error");
+    }
   }
- Future<T> insert(dynamic request) async {
-  var url = "$_baseUrl$_endpoint";
-  var uri = Uri.parse(url);
-  var headers = createHeaders();
 
-  // Dodajte provjeru je li request null prije slanja
-  var jsonRequest = request != null ? jsonEncode(request) : null;
+  Future<T> insert(dynamic request) async {
+    var url = "$_baseUrl$_endpoint";
+    var uri = Uri.parse(url);
+    var headers = createHeaders();
 
-  var response = await http.post(uri, headers: headers, body: jsonRequest);
+    // Dodajte provjeru je li request null prije slanja
+    var jsonRequest = request != null ? jsonEncode(request) : null;
 
-  if (isValidResponse(response)) {
-    var data = jsonDecode(response.body);
-    return fromJson(data);
-  } else {
-    // ignore: unnecessary_new
-    throw Exception("Unknown error");
+    var response = await http.post(uri, headers: headers, body: jsonRequest);
+
+    if (isValidResponse(response)) {
+      var data = jsonDecode(response.body);
+      return fromJson(data);
+    } else {
+      // ignore: unnecessary_new
+      throw Exception("Unknown error");
+    }
   }
-}
-   Future<T>update(int id,[dynamic request])async{
-     var url="$_baseUrl$_endpoint/$id";
-     var uri=Uri.parse(url);
-    var headers=createHeaders();
-    
-    var jsonRequest=jsonEncode(request);
-    var response=await http.put(uri,headers: headers,body: jsonRequest);
 
-     if(isValidResponse(response)){
-     var data=jsonDecode(response.body);
-     return fromJson(data);
-   }
-   else{
-        // ignore: unnecessary_new
-        throw new Exception("Unknown error");
-   }
+  Future<T> update(int id, [dynamic request]) async {
+    var url = "$_baseUrl$_endpoint/$id";
+    var uri = Uri.parse(url);
+    var headers = createHeaders();
+
+    var jsonRequest = jsonEncode(request);
+    var response = await http.put(uri, headers: headers, body: jsonRequest);
+
+    if (isValidResponse(response)) {
+      var data = jsonDecode(response.body);
+      return fromJson(data);
+    } else {
+      // ignore: unnecessary_new
+      throw new Exception("Unknown error");
+    }
   }
-Future<void> delete(int id) async {
-  var url = "$_baseUrl$_endpoint/$id";
-  var uri = Uri.parse(url);
-  var headers = createHeaders();
 
-  var response = await http.delete(uri, headers: headers);
+  Future<void> delete(int id) async {
+    var url = "$_baseUrl$_endpoint/$id";
+    var uri = Uri.parse(url);
+    var headers = createHeaders();
 
-  if (isValidResponse(response)) {
-  } else {
-    // ignore: unnecessary_new
-    throw Exception("Unknown error");
+    var response = await http.delete(uri, headers: headers);
+
+    if (isValidResponse(response)) {
+    } else {
+      // ignore: unnecessary_new
+      throw Exception("Unknown error");
+    }
   }
-}
 
-  T fromJson(data){
+  T fromJson(data) {
     throw Exception("Method not implemented");
   }
-bool isValidResponse(Response response){
-    if(response.statusCode < 299){
-        return true;
-    } else if(response.statusCode == 401){
-        throw Exception("Unauthorized");
-    } else {
-        throw Exception("Something bad happened please try again");
-    }
-}
 
-  Map<String ,String>createHeaders(){
-    String username=Authorization.username ?? "";
-    String password=Authorization.password ?? "";
+  bool isValidResponse(Response response) {
+    if (response.statusCode < 299) {
+      return true;
+    } else if (response.statusCode == 401) {
+      throw Exception("Unauthorized");
+    } else {
+      throw Exception("Something bad happened please try again");
+    }
+  }
+
+  Map<String, String> createHeaders() {
+    String username = Authorization.username ?? "";
+    String password = Authorization.password ?? "";
 
     print("passed creds: $username, $password");
 
-    String baseAuth="Basic ${base64Encode(utf8.encode('$username:$password'))}";
+    String baseAuth =
+        "Basic ${base64Encode(utf8.encode('$username:$password'))}";
 
-    var headers={
-      "Content-Type":"application/json",
-      "Authorization":baseAuth
+    var headers = {
+      "Content-Type": "application/json",
+      "Authorization": baseAuth
     };
     return headers;
-    
   }
+
   String getQueryString(Map params,
       {String prefix: '&', bool inRecursion: false}) {
     String query = '';
